e79d1a5f08dbd987007985a7f42c624ea0211490
Brayan Lopez <brxyxn.corp@live.com>
2022-01-22 09:22:29 -0600

    Added file directory structure and initializated go mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/brxyxn/go_mps_redcage
+
+go 1.16
+
+require github.com/joho/godotenv v1.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting service...")
+}
+
+/*
+  Methods
+
+  createClient
+  createAccount
+  createTransaction
+  getClient
+  getAccount
+  getTransaction
+*/
